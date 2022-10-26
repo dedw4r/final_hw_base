77f47561dc9197ee7c74c91422193504c87df98c
Edward <dedw4r@gmail.com>
2022-10-26 22:21:18 +1000

    Добавлен файл решения на Csharp

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[] array = new string[4];
+array[0] = "hello";
+array[1] = "2";
+array[2] = "world";
+array[3] = ":-)";
+
+string[] array2 = new string[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        array2[i] = array[i];
+    }
+}
+
+foreach(string s in array2)
+{
+    Console.Write($"{s} ");
+}
